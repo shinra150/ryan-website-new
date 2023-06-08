bb73a34d99451accfe21812bb4cb37fd7a6947c1
shinra150 <1519268360@qq.com>
2023-06-08 09:51:49 +0800

    1

---
+++ js/preview.js
@@ -0,0 +1,19 @@
+﻿
+
+$(function () {
+    var modal = document.getElementById("myModal");
+    var modalImg = document.getElementById("img01");
+    var captionText = document.getElementById("caption");
+    
+    var imglist = $("#show>img");
+    for (var i = 0; i < imglist.length; i++) {
+        imglist[i].onclick = function () {
+            modal.style.display = "block";
+            modalImg.src = this.src;
+            captionText.innerHTML = this.alt;
+        }
+    }
+    $(".close")[0].onclick=function () {
+        modal.style.display = "none";
+    }
+});
